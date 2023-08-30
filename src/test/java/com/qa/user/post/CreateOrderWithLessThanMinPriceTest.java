f985868d8fa82df03fcc3ab7609a33e31afe9312
pavelmal <mashino@mail.ru>
2023-08-30 14:56:11 +0800

    add case with price less than MIN

---
+++ src/test/java/com/qa/user/post/CreateOrderWithLessThanMinPriceTest.java
@@ -0,0 +1,42 @@
+package com.qa.user.post;
+
+import com.qa.api.model.Order;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static com.qa.api.Response.CustomResponse.getResponseBodyAs;
+import static com.qa.api.assertion.Body.checkResponseOrderIdIsNotEmpty;
+import static com.qa.api.assertion.Order.checkOrders;
+import static com.qa.api.assertion.StatusCode.checkStatusCode;
+import static com.qa.api.request.Request.doGet;
+import static com.qa.api.request.Request.doPost;
+import static com.qa.api.statuscode.StatusCode.OK;
+import static com.qa.api.url.OrderUrl.CREATE_ORDER;
+import static com.qa.api.url.OrderUrl.GET_ORDER;
+
+public class CreateOrderWithLessThanMinPriceTest {
+
+    @Test(description = "Create an order with less than MIN price")
+    public void createOrderWithLessThanMinPrice() {
+
+        Order createdOrder = new Order("test", Long.MIN_VALUE - 1);
+
+        Response response = doPost(CREATE_ORDER, createdOrder);
+
+        checkStatusCode(OK, response.statusCode());
+
+        Order createdOrderResponse = getResponseBodyAs(response, Order.class);
+
+        int expectedOrderId = createdOrderResponse.getId();
+
+        checkResponseOrderIdIsNotEmpty(response);
+
+        // Check order after creation by GET method
+
+        Response responseAfterCreatingOrder = doGet(GET_ORDER, expectedOrderId);
+        Order orderAfterCreation = getResponseBodyAs(responseAfterCreatingOrder, Order.class);
+        createdOrder.setId(expectedOrderId);
+
+        checkOrders(createdOrder, orderAfterCreation);
+    }
+}
