9f497c0c1ccb0daa6a857f0b7d3ae003091b0bd0
pavelmal <mashino@mail.ru>
2023-08-30 14:16:58 +0800

    add case with zero price

--- src/main/java/com/qa/api/Response/ResponseErrorMessage.java
+++ src/main/java/com/qa/api/Response/ResponseErrorMessage.java
@@ -3,4 +3,5 @@ package com.qa.api.Response;
 public class ResponseErrorMessage {
 
     public static final String NO_NEED_TO_PASS_ID = "You don't need to pass an 'id' param";
+    public static final String PRICE_LESS_OR_EQUAL_ZERO = "Price can't be less or equal than 0";
 }
