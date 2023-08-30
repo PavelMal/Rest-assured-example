2b82c9b3577e985e8e93706690a0a5b7281ade57
pavelmal <mashino@mail.ru>
2023-08-30 15:24:31 +0800

    add case to create order with whitespace name

--- src/test/java/com/qa/user/post/CreateOrderWhitespaceNameTest.java
+++ src/test/java/com/qa/user/post/CreateOrderWhitespaceNameTest.java
@@ -5,22 +5,22 @@ import com.qa.api.model.ResponseError;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-import static com.qa.api.Response.ResponseErrorMessage.NEED_TO_PASS_NAME;
+import static com.qa.api.Response.ResponseErrorMessage.EMPTY_NAME;
 import static com.qa.api.assertion.Body.checkResponseErrorEqualsExpectedError;
 import static com.qa.api.assertion.StatusCode.checkStatusCode;
 import static com.qa.api.request.Request.doPost;
 import static com.qa.api.statuscode.StatusCode.BAD_REQUEST;
 import static com.qa.api.url.OrderUrl.CREATE_ORDER;
 
-public class CreateOrderNoNameTest {
+public class CreateOrderWhitespaceNameTest {
 
-    @Test(description = "Create an order with no name")
-    public void createOrderWithNoName() {
+    @Test(description = "Create an order with whitespace name")
+    public void createOrderWithWhiteSpaceName() {
 
-        Order createdOrder = new Order(1L);
+        Order createdOrder = new Order(" ", 1L);
 
         Response response = doPost(CREATE_ORDER, createdOrder);
-        ResponseError expectedError = new ResponseError(NEED_TO_PASS_NAME);
+        ResponseError expectedError = new ResponseError(EMPTY_NAME);
 
         checkStatusCode(BAD_REQUEST, response.statusCode());
         checkResponseErrorEqualsExpectedError(response, expectedError);
